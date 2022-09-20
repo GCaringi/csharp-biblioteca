36f1dbaebb4f9e2359120833bd5e27b9a995ae6a
GCaringi <giannicaringi94@gmail.com>
2022-09-20 14:48:44 +0200

    Add Classes for library User's

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using csharp_biblioteca;
+
+Users marco = new Users("Marco", "Pietrobono", "marck@gmail.com", "prova1234", "333");
+
+Console.WriteLine(marco.Name);
+Console.WriteLine(marco.Surname);
+Console.WriteLine(marco.Email);
+Console.WriteLine(marco.Password);
