cfb60d814a6034c6f6fbe548c8678419a97fc29d
GCaringi <giannicaringi94@gmail.com>
2022-09-20 16:08:24 +0200

    Constructor of Library

--- Library.cs
+++ Library.cs
@@ -8,7 +8,21 @@ namespace csharp_biblioteca
 {
     public class Library
     {
-        
+        public string Name { get; }
+
+        public List<Book> books;
+
+        public List<Dvd> dvds;
+
+        public List<Users> users;
+
+        public Library(string name)
+        {
+            Name = name;
+            books = new List<Book>();
+            dvds = new List<Dvd>();
+            users = new List<Users>();
+        }
     }
 
     
